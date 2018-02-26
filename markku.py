6975addea4374e6631673899ae1ecbb5c7c50674
eltsu7 <eeli.hernesniemi@gmail.com>
2018-02-27 00:19:26 +0200

    kiitoscounter toimii ny

--- markku.py
+++ markku.py
@@ -11,23 +11,40 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 '''TELEGRAM KAMAA'''
 
 
+class FilterKiitos(BaseFilter):
+    def filter(self, message):
+        return "kiitos" in message.text.lower()
+
+
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="Woof woof motherfucker")
 
 
 def darkroom(bot, update):
-    print("asd")
-    # Pimiöjutut
+    reply = "Ei toimi vielä"
+    bot.send_message(chat_id=update.message.chat_id, text=reply)
+
+
+def kiitos(bot, update):
+    if update.message.from_user.username in data:
+        data[update.message.from_user.username] += 1
+    else:
+        data[update.message.from_user.username] = 1
+
+    file_write("data.json")
 
     
 def handlers(updater):
     dp = updater.dispatcher
 
+    filter_kiitos = FilterKiitos()
+
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('pimiö', darkroom))
+    dp.add_handler(MessageHandler(filter_kiitos, kiitos))
     
 
-'''MUUTA KAMAA'''
+''' MUUTA KAMAA '''
 
 
 def file_read(filename):
@@ -35,9 +52,9 @@ def file_read(filename):
 
     try:
         with open(filename, 'r') as file:
-            data = json.load(file)
+            saved_data = json.load(file)
         file.close()
-        return data
+        return saved_data
     except FileNotFoundError:
         print("Oh dog file not found")
         exit(1)
