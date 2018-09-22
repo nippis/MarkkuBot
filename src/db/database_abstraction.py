9c0cf436510fc8b8c79410172e86b14e02827791
Antti Kallonen <kallonen.antti@gmail.com>
2018-09-22 18:44:20 +0300

    Tietokannan interfacee

---
+++ src/db/database_abstraction.py
@@ -0,0 +1,31 @@
+# Bridge-patternin abstraktio-luokka
+class DatabaseAbstraction:
+    def __init__(self, imp):
+        self.imp = imp  # Toteuttava luokka
+
+    def in_blacklist(self):
+        return self.imp.in_blacklist()
+
+    def add_blacklist(self, user_id):
+        self.imp.add_blacklist(user_id)
+
+    def remove_blacklist(self, user_id):
+        self.imp.remove_blacklist(user_id)
+
+    def increment_counter(self, user_id, chat_id, counter, amount):
+        self.imp.increment_counter(user_id, chat_id, counter, amount)
+
+    def get_counter_top(self, chat_id, counter, top_amount):
+        return self.imp.get_counter_top(chat_id, counter, top_amount)
+
+    def word_collection_add(self, chat_id, user_id, chat_title, username, \
+        word, amount):
+        self.imp.word_collection_add(chat_id, user_id, chat_title, \
+            username, word, amount)
+
+    def word_collection_get_chat(self, chat_id):
+        return self.imp.word_collection_get_chat(chat_id)
+
+    def word_collection_get_chat_user(self, chat_id, user_id):
+        return self.imp.word_collection_get_chat_user(chat_id, user_id)
+
