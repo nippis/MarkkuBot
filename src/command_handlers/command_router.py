133b91f8746fd6cdeef15c44c59373705cea816a
Antti Kallonen <kallonen.antti@gmail.com>
2018-09-23 13:53:36 +0300

    help

--- src/command_handlers/command_router.py
+++ src/command_handlers/command_router.py
@@ -84,3 +84,24 @@ class CommandRouter():
         except URLError as e:
             print(e.reason)
             bot.send_message(chat_id=chat_id, text="Ei ny onnistunu (%s)" % e.reason)
+
+    def help(self, bot, update):
+        printlog(update, "help")
+
+        _, chat_id = get_ids(update)
+        count_and_write(self.db, update, "commands")
+
+        reply = "Komennot:\n" \
+                "/darkroom - Kertoo onko joku pimiöllä\n" \
+                "/stats - Chattikohtaiset statsit\n" \
+                "/toptenmsg - Chatin kovimmat viestittelijät\n"\
+                "/toptenkiitos - Chatin kovimmat kiitostelijat\n"\
+                "/noutaja - Postaa satunnaisen noutajakuvan\n"\
+                "/protip - Antaa ammatti valo kuvaus vinkin!\n"\
+                "/blacklist - Poista omat tietosi Markun tietokannasta ja estä uusien tallentaminen, lähetä privana Markulle\n"\
+                "/unblacklist - Salli omien tietojesi tallentaminen blacklist-komennon jälkeen, lähetä privana Markulle\n"\
+                "\n" \
+                "Botin koodit: @eltsu7 ja @kulmajaba\n" \
+                "Valosensorit ja siihen koodit: @anttimoi"
+
+        bot.send_message(chat_id=chat_id, text=reply)            
