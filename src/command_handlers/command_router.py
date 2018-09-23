6c92a3aa6dbea87c6cc0d59e9cc9588566108f6e
Antti Kallonen <kallonen.antti@gmail.com>
2018-09-23 13:48:56 +0300

    darkroom

--- src/command_handlers/command_router.py
+++ src/command_handlers/command_router.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import json
+from urllib.request import urlopen
+from urllib.error import URLError
 
 from core.printlog import printlog
 from core.count_and_write import count_and_write
@@ -48,3 +51,36 @@ class CommandRouter():
         msg += "\nPhotos: {}".format(count_photos)        
 
         bot.send_message(chat_id=chat_id, text=msg)
+
+    # Lukee netistä valosensorin datan ja kertoo onko kerhohuoneella valot päällä
+    def darkroom(self, bot, update):
+        printlog(update, "darkroom")
+
+        _, chat_id = get_ids(update)
+        count_and_write(self.db, update, "commands")
+        
+        try:
+            with urlopen("https://ttkamerat.fi/darkroom/api/v1/sensors/latest") as url:
+                sensor_data = json.loads(url.read().decode())
+
+                value_light = 0
+                isDarkroomPopulated = False
+
+                # JSON härössä muodossa, sen takia teemme näin. Esimerkki:
+                #   {"entries": [{"value": 191, "sensor": "light1", "inserted": "2018-07-27T16:18:43.589Z"}]}
+                for sensor in sensor_data["entries"]:
+                    if sensor["sensor"] == "light1":
+                        value_light = sensor["value"]
+
+                if value_light > 100:
+                    isDarkroomPopulated = True
+
+                if isDarkroomPopulated:
+                    reply = "Joku on pimiöllä :O\n"
+                else:
+                    reply = "Pimiö tyhjä :(\n"
+
+                bot.send_message(chat_id=chat_id, text=reply)
+        except URLError as e:
+            print(e.reason)
+            bot.send_message(chat_id=chat_id, text="Ei ny onnistunu (%s)" % e.reason)
