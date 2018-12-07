e3fff2094b918e80f441001f1331a007b0449a40
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2018-12-08 01:35:48 +0200

    protip ja camera_versus commandrouterin sisään. masterlist cr:n muuttujaksi

--- src/core/camera_versus_text.py
+++ src/core/camera_versus_text.py
@@ -1,10 +1,11 @@
 import random
-#TODO masterlist
+from core.file_read import file_read
 
 def camera_versus_text():
     # Painotettu lista random pickeihin
 
-    global camera_list
+    camera_list = file_read("src/masterlist.json")["Cameras"]
+
     weighted_camera_list = camera_list["Common"] * 10 + camera_list["Medium"] * 3 + camera_list["Rare"]
             
     return "{} vai {}?".format(random.choice(weighted_camera_list), random.choice(weighted_camera_list))
