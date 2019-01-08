723c9cf6bd56166cc1f34b9d4096bd508b2c533f
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2019-01-08 15:26:21 +0200

    protipsei, kameroita, hallitushuutelut pois. kamerakyssäreitä painotettu uudelleen ja tuplavaihtoehto poistettu

--- src/core/camera_versus_text.py
+++ src/core/camera_versus_text.py
@@ -6,6 +6,13 @@ def camera_versus_text():
 
     camera_list = masterlist.cameras
 
-    weighted_camera_list = camera_list["Common"] * 10 + camera_list["Medium"] * 3 + camera_list["Rare"]
+    weighted_camera_list = camera_list["Common"] * 20 + camera_list["Medium"] * 5 + camera_list["Rare"]
+
+    camera1 = random.choice(weighted_camera_list)
+
+    # Poistetaan ensimmäinen valinta listasta ettei tule tuplia
+    weighted_camera_list = [x for x in weighted_camera_list if x != camera1]
+
+    camera2 = random.choice(weighted_camera_list)
             
-    return "{} vai {}?".format(random.choice(weighted_camera_list), random.choice(weighted_camera_list))
+    return "{} vai {}?".format(camera1, camera2)
