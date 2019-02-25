7c722f19576a1e15085c1166dc39956d41237827
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2019-02-25 18:04:52 +0200

    muutetaan backupscriptit oikeaan jsonmuotoon

--- docker_mongo_backup.sh
+++ docker_mongo_backup.sh
@@ -11,9 +11,9 @@ BLACKLIST_FILE=$(date '+%N')'_temp_bl.json'
 BACKUP_FILE='MarkkuBot_Backup_'$(date '+%Y-%m-%d_%H-%M-%S')'.tar.gz'
 
 # Execute mongo export inside db container
-docker exec $CONTAINER sh -c 'mongoexport --db '$DB_NAME' --collection '$CHATS_COLL_NAME'' > $CHATS_FILE
-docker exec $CONTAINER sh -c 'mongoexport --db '$DB_NAME' --collection '$WORDS_COLL_NAME'' > $WORDS_FILE
-docker exec $CONTAINER sh -c 'mongoexport --db '$DB_NAME' --collection '$BLACKLIST_COLL_NAME'' > $BLACKLIST_FILE
+docker exec $CONTAINER sh -c 'mongoexport --jsonarray --db '$DB_NAME' --collection '$CHATS_COLL_NAME'' > $CHATS_FILE
+docker exec $CONTAINER sh -c 'mongoexport --jsonarray --db '$DB_NAME' --collection '$WORDS_COLL_NAME'' > $WORDS_FILE
+docker exec $CONTAINER sh -c 'mongoexport --jsonarray --db '$DB_NAME' --collection '$BLACKLIST_COLL_NAME'' > $BLACKLIST_FILE
 
 # Make tar and remove temp files
 tar cvzf $BACKUP_FILE $CHATS_FILE $WORDS_FILE $BLACKLIST_FILE
