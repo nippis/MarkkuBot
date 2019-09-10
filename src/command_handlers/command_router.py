9c47691f748a7df0d1655bcc88d509f67bc581c4
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2019-09-10 11:10:46 +0300

    ajatusvirhe datediffin vertailussa korjattu darkroom komennossa

--- src/command_handlers/command_router.py
+++ src/command_handlers/command_router.py
@@ -123,11 +123,10 @@ class CommandRouter():
 
                     date_diff = current_datetime - inserted_datetime
 
-                    if date_diff.seconds > 3600:
-                        if date_diff.days == 0:
-                            reply += " ({} tuntia sitten)".format(date_diff.seconds//3600)
-                        else:
-                            reply += " ({} päivää ja {} tuntia sitten)".format(date_diff.days, date_diff.seconds//3600)
+                    if date_diff.seconds > 3600 and date_diff.days == 0:
+                        reply += " ({} tuntia sitten)".format(date_diff.seconds//3600)
+                    else if date_diff.days != 0:
+                        reply += " ({} päivää ja {} tuntia sitten)".format(date_diff.days, date_diff.seconds//3600)
 
                 else:
                     reply = "Ei tietoa :/"
